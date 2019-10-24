1cb47dbbdece624ed78f915e5c92d332db6b1afc
Tom Kleyntsens <tkleynts@w-r9-p4.s19.be>
2019-10-24 14:14:03 +0200

    pushing for correction

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,13 +6,13 @@
 /*   By: tkleynts <tkleynts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 17:07:29 by tkleynts          #+#    #+#             */
-/*   Updated: 2019/10/24 13:32:23 by tkleynts         ###   ########.fr       */
+/*   Updated: 2019/10/24 14:12:09 by tkleynts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void *(*del)(void *))
 {
 	t_list	*liste2;
 	t_list	*new_elem;
@@ -22,7 +22,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
 		return (NULL);
 	while (lst)
 	{
-		new_elem = ft_lstnew((*f)(lst->content));
+		if(!(new_elem = ft_lstnew((*f)(lst->content))))
+		{
+			ft_lstclear(&liste2, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&liste2, new_elem);
 		lst = lst->next;
 	}
