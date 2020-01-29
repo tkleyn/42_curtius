db748ae1a86d2d1aa30dd158b4389659765e974a
Tom Kleyntsens <tkleynts@e-r3-p3.s19.be>
2020-01-29 13:40:08 +0100

    corrected bug %s "%"

--- conv_s.c
+++ conv_s.c
@@ -6,7 +6,7 @@
 /*   By: tkleynts <tkleynts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 15:25:06 by tkleynts          #+#    #+#             */
-/*   Updated: 2020/01/27 17:43:56 by tkleynts         ###   ########.fr       */
+/*   Updated: 2020/01/29 13:33:37 by tkleynts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ char			*conv_s(t_utils *data, t_flags *flgs, char *str)
 	int check;
 
 	check = 1;
-	if (flgs->prec == 0 && ft_strncmp(str, "%", 2) && check--
-						&& !(str = (ft_strdup(""))))
+	if (flgs->prec == 0 && check-- && !(str = (ft_strdup(""))))
 		return (NULL);
 	if (str && check && !(str = (ft_strdup(str))))
 		return (NULL);
